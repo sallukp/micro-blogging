90fa0c942e28e4c835c077d0b0eca4eb21489f6e
sallukp <salmon1989@gmail.com>
2020-10-25 01:25:27 +0200

    [RESOURCES] app cons added and refactor

---
+++ app/src/main/java/me/salmon/microblog/SplashActivity.kt
@@ -0,0 +1,11 @@
+package me.salmon.microblog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
